f8c5ee0360744e4decf205c09c5955061cc181a2
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-05-30 23:38:45 -0300

    new

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
 import './new.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
 const New = () => {
   return (
-    <div>New</div>
+    <div className='new'>
+      <Sidebar/>
+      <div className="newContainer">
+        <Navbar/>
+        <div className="top">
+          <h1 className="">Add new user</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt=""/>
+          </div>
+          <div className="right">
+            <form action="">
+              <div className="formInput">
+                <label htmlFor='file'>
+                  Image: <DriveFolderUploadIcon className='icon'/>
+                </label>
+                <input type="file" id='file' style={{display:'none'}}/>
+              </div>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder='john_doe'/>
+              </div>
+              <div className="formInput">
+                <label>Name and surname</label>
+                <input type="text" placeholder='John Doe'/>
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder='john_doe@gmail.com'/>
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="email" placeholder='+1 234 567 89'/>
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password"/>
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder='Elton St. 216 NewYork'/>
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder='USA'/>
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
